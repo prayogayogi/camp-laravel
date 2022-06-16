0e4418667d871bb11956358048bba0c11e98c809
prayogayogi <yogikpiter@gmail.com>
2022-06-16 16:12:39 +0700

    integrasi landing page, checkout, and my dashboard

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -2,13 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Checkout extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'cvc', 'is_paid'];
+
+    public function setExpiredAttribute($value)
+    {
+        $this->attributes["expired"] = date("y-m-t", strtotime($value));
+    }
+
+    /**
+     * Get the Camp that owns the Checkout
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Camp(): BelongsTo
+    {
+        return $this->belongsTo(Camp::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,20 +20,22 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/checkout', function () {
-    return view('checkout');
-})->name('checkout');
-
-Route::get('/success_checkout', function () {
-    return view('success_checkout');
-})->name('success_checkout');
-
 // Socialite route
 Route::get('sign-to-google', [UserController::class, 'google'])->name('sign.to.google');
 Route::get('auth/google/callback', [UserController::class, 'handeleProviderCallback'])->name('user.google.callback');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(["auth"])->group(function () {
+    // Checout route
+    Route::get("/checkout/success", [CheckoutController::class, "success"])->name("success_checkout");
+    Route::get("/checkout/{camp:slug}", [CheckoutController::class, "create"])->name("checkout.create");
+    Route::post("/checkout/{camp}", [CheckoutController::class, "store"])->name("checkout.store");
+
+    // user dashboard
+    Route::get("/dashboard", [HomeController::class, "dashboard"])->name("dashboard");
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
