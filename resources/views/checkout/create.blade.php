d53aaf056cf557fcb12d4d9e062ab980005eb4d6
prayogayogi <yogikpiter@gmail.com>
2022-06-18 14:00:34 +0700

    Validation

--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -32,29 +32,47 @@
                             @csrf
                             <div class="mb-4">
                                 <label class="form-label">Full Name</label>
-                                <input name="name" type="text" class="form-control" value="{{ Auth::user()->name; }}"/>
+                                <input name="name" type="text" class="form-control{{ $errors->has("name") ? " is-invalid" : "" }}" value="{{ Auth::user()->name; }}" required/>
+                                @if ($errors->has("name"))
+                                    <span class="text-danger">{{ $errors->first("name") }}</span>
+                                @endif
                             </div>
                             <div class="mb-4">
                                 <label class="form-label">Email Address</label>
-                                <input name="email" type="email" class="form-control" value="{{ Auth::user()->email; }}"/>
-                            </div>
+                                <input name="email" type="email" class="form-control{{ $errors->has("email") ? " is-invalid" : "" }}" value="{{ Auth::user()->email; }}" required/>
+                                @error('email')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                                </div>
                             <div class="mb-4">
                                 <label class="form-label">Occupation</label>
-                                <input name="occupation" type="text" class="form-control" value="{{ Auth::user()->occupation; }}"/>
+                                <input name="occupation" type="text" class="form-control @error('occupation') is-invalid @enderror" value="{{ old("occupation") ? : Auth::user()->occupation; }}" required/>
+                                @error('occupation')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
                             <div class="mb-4">
                                 <label class="form-label">Card Number</label>
-                                <input name="card_number" type="number" class="form-control"/>
+                                <input name="card_number" type="number" class="form-control @error("card_number") is-invalid @enderror" value="{{ old("card_number") }}" required/>
+                                @error('card_number')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
                             <div class="mb-5">
                                 <div class="row">
                                     <div class="col-lg-6 col-12">
                                         <label class="form-label">Expired</label>
-                                        <input name="expired" type="month" class="form-control"/>
+                                        <input name="expired" type="month" class="form-control @error("expired") is-invalid @enderror" required/>
+                                        @error('expired')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
                                     </div>
                                     <div class="col-lg-6 col-12">
                                         <label class="form-label">CVC</label>
-                                        <input name="cvc" type="number" maxlength="3" class="form-control"/>
+                                        <input name="cvc" type="number" maxlength="3" class="form-control @error("cvc") is-invalid @enderror" value="{{ old("cvc") }}" required/>
+                                        @error('cvc')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
                                     </div>
                                 </div>
                             </div>
